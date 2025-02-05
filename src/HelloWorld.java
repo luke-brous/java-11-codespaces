14fa8c038fd2b07faa7efd0bd89a81c857713d8f
Luke <brouslr0@sewanee.edu>
2025-02-05 15:38:19 +0000

    some changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,12 @@
+
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Codespaces!");
+        
+        for(int i = 0;i<10;i++){
+            System.out.println("Hello, Codespaces!");
+            
+        }
+        
     }
 }
